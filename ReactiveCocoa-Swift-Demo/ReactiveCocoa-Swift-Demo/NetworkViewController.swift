03c93ba3176e53306ae4653631596e623aa9e752
Ash Furrow <ash@ashfurrow.com>
2014-10-27 21:43:20 +0100

    Minified.

--- ReactiveCocoa-Swift-Demo/ReactiveCocoa-Swift-Demo/NetworkViewController.swift
+++ ReactiveCocoa-Swift-Demo/ReactiveCocoa-Swift-Demo/NetworkViewController.swift
@@ -18,22 +18,22 @@ class NetworkViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        RAC(imageView, "image") <~ goButton.rac_signalForControlEvents(.TouchUpInside).take(1).doNext({ (_) -> Void in
+        RAC(imageView, "image") <~ goButton.rac_signalForControlEvents(.TouchUpInside).take(1).doNext { (_) -> Void in
             println("Starting download")
             UIApplication.sharedApplication().networkActivityIndicatorVisible = true
-        }).then({ () -> RACSignal! in
+        }.then { () -> RACSignal! in
             let url = NSURL(string: location)!
             let request = NSURLRequest(URL: url)
             let networkSignal = NSURLConnection.rac_sendAsynchronousRequest(request).deliverOn(RACScheduler.mainThreadScheduler())
             return networkSignal
-        }).map(toImage).doNext({ (_) -> Void in
+        }.map(toImage).doNext { (_) -> Void in
             println("Downloaded")
-        }).catch({ (error) -> RACSignal! in
+        }.catch { (error) -> RACSignal! in
             println("Encountered error: \(error)")
             return RACSignal.`return`(UIImage(named: "error"))
-        }).doNext({ (_) -> Void in
+        }.doNext { (_) -> Void in
             UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-        })
+        }
     }
 }
 
